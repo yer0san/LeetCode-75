ab165fc77fc08686657812a164358c3614b48439
Yerosan Bekele <154458965+yer0san@users.noreply.github.com>
2025-08-26 21:17:03 +0300

    Create 06-greatest-common-divisor-string.py

---
+++ array-string/06-greatest-common-divisor-string.py
@@ -0,0 +1,14 @@
+# difficulty : easy, lol not easy at all 
+# used euclid's algorithm
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if (str1 + str2 != str2 + str1):
+            return ""
+
+        def gcd(x, y):
+            while y:
+                x, y = y, x % y
+            return x
+
+        return str1[:gcd(len(str1), len(str2))]
