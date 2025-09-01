01a28b2772a45fbd9a6d3fc2da65cae069a8a7fb
Yerosan Bekele <154458965+yer0san@users.noreply.github.com>
2025-09-01 12:53:41 +0300

    Create 01-maximum-average-subarray-I.py

---
+++ sliding-window/01-maximum-average-subarray-I.py
@@ -0,0 +1,15 @@
+# difficulty : easy
+
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        l = 0
+        r = k
+        s = sum(nums[l:r])
+        res = s/k
+        while r < len(nums):
+            s = s - nums[l] + nums[r]
+            res = max(res, s/k)
+            l, r = l+1, r+1
+        return res
+
+        
